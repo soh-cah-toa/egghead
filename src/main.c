aa005f27642c900bd35a01d09bacb47f4010d750
Kevin Polulak <kpolulak@gmail.com>
2012-05-31 22:07:20 -0400

    Added check to verify that a valid brainfuck file was given. That is, it has either a .bf or .b file extension.

--- src/main.c
+++ src/main.c
@@ -17,18 +17,33 @@
 #include "egghead.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int
 main(int argc, char *argv[])
 {
+    char *filename = argv[1];
+
     /* Begin compilation if file was given. Otherwise, exit program. */
     if (argc < 2) {
         fprintf(stderr, "[ERROR] No input file given.\n");
         exit(EXIT_FAILURE);
     }
     else {
-        egghead_eval_file(argv[1]);
+        /* Proceed only if a valid .bf or .b file was given. */
+        if ((strcmp(strrchr(filename, '.'), ".bf") == 0)
+            || strcmp(strrchr(filename, '.'), ".b") == 0) {
+
+            egghead_eval_file(filename);
+        }
+        else {
+            fprintf(stderr,
+                    "[ERROR] File %s is not a valid brainfuck file.\n",
+                    filename);
+
+            exit(EXIT_FAILURE);
+        }
     }
 
     return 0;
